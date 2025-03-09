5ede015f06164b9cef3e541fa92428274eecebc3
aymennasri <aymennasrii@proton.me>
2025-03-09 19:52:42 +0100

    Update grammars.r version + tweak highlighting

--- languages/r/outline.scm
+++ languages/r/outline.scm
@@ -1,30 +1,30 @@
-; Variables
-(left_assignment
-   name: (identifier) @name
-   value: (_) @value) @item
+; ; Variables
+; (left_assignment
+;    name: (identifier) @name
+;    value: (_) @value) @item
 
-(equals_assignment
-   name: (identifier) @name
-   value: (_) @value) @item
+; (equals_assignment
+;    name: (identifier) @name
+;    value: (_) @value) @item
 
-(right_assignment
-    value: (_) @value
-    name: (identifier) @name) @item
+; (right_assignment
+;     value: (_) @value
+;     name: (identifier) @name) @item
 
-(super_assignment
-    name: (identifier) @name
-    value: (_) @value) @item
+; (super_assignment
+;     name: (identifier) @name
+;     value: (_) @value) @item
 
-(super_right_assignment
-    value: (_) @value
-    name: (identifier) @name) @item
+; (super_right_assignment
+;     value: (_) @value
+;     name: (identifier) @name) @item
 
-; `for` loop
-(for
-  "(" (identifier) @name
-      "in" (_) @value
-  ")"
-  body: (_)) @item
+; ; `for` loop
+; (for
+;   "(" (identifier) @name
+;       "in" (_) @value
+;   ")"
+;   body: (_)) @item
 
 ; Jupyter cell tag
 (
