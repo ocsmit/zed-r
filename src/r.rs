5735a37ae7b7a2828dd300ac5f39423a367094aa
Alex D Hill <me@alexdhill.com>
2026-03-02 21:48:02 -0800

    Launching air formatter as languageserver

--- src/r.rs
+++ src/r.rs
@@ -10,22 +10,32 @@ impl zed::Extension for RExtension {
 
     fn language_server_command(
         &mut self,
-        _language_server_id: &LanguageServerId,
+        language_server_id: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
+        let Some(air_path) = worktree.which("air") else {
+            return Err("air unavailable - falling back to styler".to_string());
+        };
         let Some(r_path) = worktree.which("R") else {
             return Err("R not available".to_string());
         };
-
-        Ok(zed::Command {
-            command: r_path.to_string(),
-            args: vec![
-                "--slave".to_string(),
-                "-e".to_string(),
-                "languageserver::run()".to_string(),
-            ],
-            env: Default::default(),
-        })
+        match language_server_id.as_ref() {
+            "air" => Ok(zed::Command {
+                command: air_path.to_string(),
+                args: vec!["language-server".into()],
+                env: Default::default(),
+            }),
+            "r_language_server" => Ok(zed::Command {
+                command: r_path.to_string(),
+                args: vec![
+                    "--slave".to_string(),
+                    "-e".to_string(),
+                    "languageserver::run()".to_string(),
+                ],
+                env: Default::default(),
+            }),
+            id => Err(format!("Unknown language server: {id}")),
+        }
     }
 }
 
